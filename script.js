167bd6ce405a48e4963b9c89d9355e28feffcc27
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-10-30 16:55:09 -0400

    Add event listeners for operators and numerals
    Fix id names for number buttons
    
    Add additional css styling

--- script.js
+++ script.js
@@ -4,6 +4,65 @@ let previousInputs = [];
 let currentOperator = '';
 
 // Add in event listeners
+const addition = document.querySelector('#addition');
+addition.addEventListener('click', () => {
+  operatorClick('+');
+});
+const subtraction = document.querySelector('#subtraction');
+subtraction.addEventListener('click', () => {
+  operatorClick('-');
+});
+const multiplication = document.querySelector('#multiplication');
+multiplication.addEventListener('click', () => {
+  operatorClick('*');
+});
+const division = document.querySelector('#division');
+division.addEventListener('click', () => {
+  operatorClick('/');
+});
+
+const one = document.querySelector('#one');
+one.addEventListener('click', () => {
+  numberClick(1);
+});
+const two = document.querySelector('#two');
+two.addEventListener('click', () => {
+  numberClick(2);
+});
+const three = document.querySelector('#three');
+three.addEventListener('click', () => {
+    numberClick(3);
+});
+
+const four = document.querySelector('#four');
+four.addEventListener('click', () => {
+  numberClick(4);
+});
+const five = document.querySelector('#five');
+five.addEventListener('click', () => {
+  numberClick(5);
+});
+const six = document.querySelector('#six');
+six.addEventListener('click', () => {
+    numberClick(6);
+});
+
+const seven = document.querySelector('#seven');
+seven.addEventListener('click', () => {
+  numberClick(7);
+});
+const eight = document.querySelector('#eight');
+eight.addEventListener('click', () => {
+  numberClick(8);
+});
+const nine = document.querySelector('#nine');
+nine.addEventListener('click', () => {
+    numberClick(9);
+});
+const zero = document.querySelector('#zero');
+zero.addEventListener('click', () => {
+    numberClick(0);
+});
 
 // Functions
 function updateDisplay() {
@@ -25,9 +84,12 @@ function operatorClick(operator) {
     // Reset currentInput
     currentInput = '';
     updateDisplay();
+    console.log(previousInputs)
 };
 
-// Testing for functionality
-let testInput = prompt("Enter a number");
-numberClick(testInput)
-operatorClick('multiply')
+function calculate() {
+    // Append most recent input
+    previousInputs.push(currentInput);
+    
+    updateDisplay();
+}
